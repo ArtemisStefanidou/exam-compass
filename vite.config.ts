d0cf4befeb70f01cb1240f488eaf2d41178eaac1
artemis <astefanidou@hua.gr>
2026-01-29 17:45:56 +0200

    fix: set vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(() => ({
-  base: "/exam-compass/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: "/exam-compass/", 
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
