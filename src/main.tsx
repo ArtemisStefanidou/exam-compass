10cfc37e8b5ab5d6b130198e0338df1aa39af764
artemis <astefanidou@hua.gr>
2026-01-29 17:10:07 +0200

    fix: wrap app with AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { AuthProvider } from "@/lib/auth";
 
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </HashRouter>
 );
